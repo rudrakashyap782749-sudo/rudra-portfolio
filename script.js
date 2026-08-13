f8a053a3c6262bc284ae3d30850bf80d20b53a2a
rudrakashyap782749-sudo <rudrakashyap782749@gmail.com>
2026-08-13 13:25:56 +0530

    Update welcome message and add password check function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function showMessage() {
-  alert('Welcome to Ethical Hacking Learning Website!');
+  alert('Hello! Main Rudra hoon aur mobile se coding seekh raha hoon.');
+}
+function showMessage() {
+  alert('Hello! Main Rudra hoon aur mobile se coding seekh raha hoon.');
+}
+function checkPassword() {
+  let pass = document.getElementById("password").value;
+  let result = document.getElementById("result");
+
+  if (pass.length < 6) {
+    result.innerHTML = "❌ Weak Password";
+  } else if (pass.length < 10) {
+    result.innerHTML = "⚠️ Medium Password";
+  } else {
+    result.innerHTML = "✅ Strong Password";
+  }
+}
+function toggleTheme() {
+  document.body.classList.toggle("light");
 }
